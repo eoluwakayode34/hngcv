346ab512de4defef1c3c79d4d5a38203090e029c
Olagundoye Emmanuel <40691457+eoluwakayode34@users.noreply.github.com>
2021-08-17 23:05:25 +0100

    first commmit

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById('menu').addEventListener('click', ( )=> {
+
+
+    document.getElementById('nav').classList.toggle('nav-toggle')
+})
+
+
+document.getElementById('myname').innerText = "I'm Olagundoye Emmanuel"
+
+
+// print name
+console.log('Olagundoye Emmanuel')
